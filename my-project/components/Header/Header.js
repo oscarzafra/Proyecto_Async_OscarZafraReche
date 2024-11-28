f2cb2677ae91551192b4a2281f7feb727fb0c080
oscarzafra <oscarzafra35@gmail.com>
2024-11-28 23:10:59 +0100

    Proyecto Terminado

--- my-project/components/Header/Header.js
+++ my-project/components/Header/Header.js
@@ -4,6 +4,13 @@ const template = () => `
 <h1>Pinterest</h1>
 <input type="text" id="searchInput" placeholder="Ex: rollercoaster" />
 <button id="searchBtn">Search</button>
+<ol id="holi">
+        <li id="navbar">Inicio</li>
+        <li id="navbar">Trending Topic</li>
+        <li id="navbar">Imágenes Guardadas</li>
+        <li id="navbar">Foro</li>
+        <li id="navbar">Mi Perfil</li>
+    </ol>
 <button id="theme-toggle">Cambiar Tema</button>
 `;
 
@@ -12,3 +19,4 @@ const Header = () => {
 };
 
 export default Header;
+
